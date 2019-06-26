bb94726fc014a74452818d012cbae2c8e776a531
jan-grasewicz <j.grasewicz@gmail.com>
2019-06-26 19:56:55 +0200

    player displays list of videos and chosen video

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,11 +1,43 @@
-import * as React from 'react'
+import React, { useState, useEffect } from "react";
 
-const Player = () => {
-  return(
+function Player({ portfolio }) {
+  const [active, setActive] = useState({});
+
+  const initialState = portfolio[3];
+
+  useEffect(() => {
+    setActive(initialState);
+  }, [initialState]);
+
+  const handleClick = (e, video) => {
+    setActive(video);
+  };
+
+  return (
     <React.Fragment>
-      <h3>player</h3>
+      <iframe
+        src={active && active.url}
+        width="640"
+        height="320"
+        frameBorder="0"
+        allow="autoplay; fullscreen"
+        allowFullScreen
+      />
+      <ul>
+        {portfolio &&
+          portfolio.map(video => (
+            <li key={video.url}>
+              <p
+                onClick={e => handleClick(e, video)}
+                style={{ color: active && video.url === active.url && "red" }}
+              >
+                {video.title}
+              </p>
+            </li>
+          ))}
+      </ul>
     </React.Fragment>
-  )
+  );
 }
 
-export default Player
+export default Player;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Player from "./Player";
 
-
 function Portfolio() {
+  const [portfolio, setPortfolio] = useState([]);
+  useEffect(() => {
+    fetch(`${process.env.PUBLIC_URL}/data.json`)
+      .then(response => response.json())
+      .then(data => setPortfolio(data.portfolio))
+//clean up todo
+    return () => {};
+  }, []);
+
   return (
     <section className="portfolio">
       <h2>Portfolio</h2>
-      <Player />
+      <Player portfolio={portfolio} />
     </section>
   );
 }
