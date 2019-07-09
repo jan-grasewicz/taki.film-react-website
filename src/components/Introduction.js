669739cdbb1054bacb60de3775610febcb5d4190
jan-grasewicz <j.grasewicz@gmail.com>
2019-07-09 23:35:00 +0200

    Introduction section styled

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,4 +1,4 @@
-import React, {useContext} from "react"
+import React, { useContext } from "react"
 import { SiteContext } from "../contexts/SiteContext"
 import styled from "styled-components"
 
@@ -7,24 +7,25 @@ function Introduction() {
   return (
     <section className="introduction">
       <Title>
-        Cześć,
-        <br /> tu taki.film
+        Cześć, <br />
+        tu taki.film
       </Title>
-      <div className="showreel">
-        <iframe
-          src={showreel.length&&showreel[0].url}
-          max-width="640"
-          height="320"
+      <ShowreelWrapper>
+        <Showreel
+          src={showreel.length && showreel[0].url}
           frameBorder="0"
           allow="autoplay; fullscreen"
           allowFullScreen
         />
-      </div>
-      <h3>taki.film czyli jaki?</h3>
-      <p>
-        Taki który przyciąga uwagę, zapada w pamięć i wyróżnia się jakością.
-      </p>
-      <button>NAPISZ DO NAS</button>
+      </ShowreelWrapper>
+      <Text>
+        <Header>taki.film czyli jaki?</Header>
+        <Descriotion>
+          Taki który przyciąga uwagę, zapada w pamięć i&nbsp;wyróżnia się
+          jakością.
+        </Descriotion>
+      <Button>NAPISZ DO NAS</Button>
+      </Text>
     </section>
   )
 }
@@ -35,5 +36,44 @@ const Title = styled.h1`
   line-height: 2.9444rem;
   padding: 2.5rem 0rem 1.3333rem;
 `
+const ShowreelWrapper = styled.div`
+  position: relative;
+  padding-top: 50%; /* video aspect ratio */
+  overflow: hidden;
+`
+const Showreel = styled.iframe`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border: 0;
+`
+const Text = styled.div`
+  text-align: left;
+  padding-top: 1.2222rem;
+`
+const Header = styled.h3`
+  font-size: ${props => props.theme.fontSize.s};
+  font-weight: 800;
+  line-height: 1.1111rem;
+  padding-bottom: 0.2778rem;
+`
+const Descriotion = styled.p`
+  font-size: ${props => props.theme.fontSize.xs};
+  font-weight: 400;
+  line-height: 1rem;
+`
+const Button = styled.button`
+  font-size: ${props => props.theme.fontSize.xs};
+  font-weight: 800;
+  line-height: 1rem;
+  color: ${props => props.theme.color.white};
+  background-color: ${props => props.theme.color.lightblue};
+  border: none;
+  width: 100%;
+  height: 3.6111rem;
 
+  margin: 1.6667rem 0px 2.2222rem;
+`
 export default Introduction
