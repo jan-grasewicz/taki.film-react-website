d553db7c8827715a19c5cef7aebe0c1daac32e2c
jan-grasewicz <j.grasewicz@gmail.com>
2019-07-18 22:15:34 +0200

    wip styling

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,22 +1,35 @@
-import React from 'react'
+import React from "react"
+import styled from "styled-components"
 
 function AboutUs() {
   return (
     <section className="aboutUs">
-    <h3>mini-carousel</h3>
-    <img
-      // src={process.env.PUBLIC_URL + "/img/Miniatura1.jpg"}
-      alt={"taki.film at work"}
-    />
-    <h3>Kim jesteśmy?</h3>
-    <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga,
-      earum! Iure provident consequatur maxime, nesciunt, molestiae
-      velit repellat delectus, debitis natus voluptas laborum nisi
-      numquam fugit illum sapiente ad suscipit.
-    </p>
-  </section>
+      <LogoCarousel>{"<<  Logo-Carousel  >>"}</LogoCarousel>
+      <Photo
+        src={process.env.PUBLIC_URL + "/img/Miniatura1.jpg"}
+        alt={"taki.film at work"}
+      />
+      <h3>Kim jesteśmy?</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, earum!
+        Iure provident consequatur maxime, nesciunt, molestiae velit repellat
+        delectus, debitis natus voluptas laborum nisi numquam fugit illum
+        sapiente ad suscipit.
+      </p>
+    </section>
   )
 }
+
+const LogoCarousel = styled.h3`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: ${props => props.theme.color.lightGray};
+  height: 56px;
+`
+const Photo = styled.img`
+  padding-top: 1.875rem;
+  width: 100%;
+`
 
 export default AboutUs
--- src/components/Player/PlayerCredits.js
+++ src/components/Player/PlayerCredits.js
@@ -1,18 +1,44 @@
-import React from "react";
+import React from "react"
+import styled from "styled-components"
 
 function PlayerCredits({ credits: { cameraMan, lights, sound, editor } }) {
   return (
-    <ul>
-      <li>camera man: {cameraMan}</li>
-      <li>lights: {lights}</li>
-      <li>sound: {sound}</li>
-      <li>editor: {editor}</li>
-    </ul>
-  );
+    <Container>
+      <List>
+        <Item>camera man: {cameraMan}</Item>
+        <Item>lights: {lights}</Item>
+        <Item>sound: {sound}</Item>
+        <Item>editor: {editor}</Item>
+      </List>
+      <DividerLine />
+      <List>
+        <Item>camera man: {cameraMan}</Item>
+        <Item>lights: {lights}</Item>
+        <Item>sound: {sound}</Item>
+        <Item>editor: {editor}</Item>
+      </List>
+    </Container>
+  )
 }
 
 PlayerCredits.defaultProps = {
   credits: {}
-};
+}
+const Container = styled.div`
+  padding-top: 0.9375rem;
+  padding-bottom: 3.25rem;
+`
+const List = styled.ul`
+  color: ${props => props.theme.color.white};
+  font-size: ${props => props.theme.fontSize.s};
+  text-align: left;
+`
+const Item = styled.li``
+const DividerLine = styled.div`
+  background-color: ${props => props.theme.color.darkGray};
+  width: 100%;
+  height: 1px;
+  margin: 2.8125rem 0rem 1.5rem;
+`
 
-export default PlayerCredits;
+export default PlayerCredits
