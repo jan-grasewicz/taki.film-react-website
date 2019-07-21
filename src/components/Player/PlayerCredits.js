6ae9db9de1ed242e394fc3688abb02b27907ff1c
jan-grasewicz <j.grasewicz@gmail.com>
2019-07-21 14:21:13 +0200

    wip table styling

--- src/components/Player/PlayerCredits.js
+++ src/components/Player/PlayerCredits.js
@@ -3,25 +3,30 @@ import styled from "styled-components"
 
 function PlayerCredits({ credits }) {
   return (
-    <Container>
-      <table>
-        <tbody>
-        {credits.length &&
-          credits.map(section =>
-            section.map(credit => (
-              <tr key={credit.name}>
+    <StyledTable>
+      {credits &&
+        credits.map((section, index) => (
+          <tbody key={index}>
+            {index !== 0 && (
+              <tr>
+                <td colSpan="2">
+                  <DividerLine />
+                </td>
+              </tr>
+            )}
+            {section.map(credit => (
+              <tr key={`${credit.name}-row`}>
                 <td>{credit.name}</td>
                 <td>
                   {credit.entrys.map(entry => (
-                    <div key={entry}>{entry}</div>
+                    <div key={`${entry}-cell`}>{entry}</div>
                   ))}
                 </td>
               </tr>
-            ))
-          )}
+            ))}
           </tbody>
-      </table>
-    </Container>
+        ))}
+    </StyledTable>
   )
 }
 
@@ -29,21 +34,19 @@ PlayerCredits.defaultProps = {
   credits: []
 }
 
-const Container = styled.div`
-  padding-top: 0.9375rem;
-  padding-bottom: 3.25rem;
+const StyledTable = styled.table`
+  margin-top: 0.9375rem;
+  margin-bottom: 3.25rem;
+  width: 100%;
+  color: ${props => props.theme.color.white};
+  font-size: ${props => props.theme.fontSize.s};
+  text-align: left;
 `
-// const List = styled.ul`
-//   color: ${props => props.theme.color.white};
-//   font-size: ${props => props.theme.fontSize.s};
-//   text-align: left;
-// `
-// const Item = styled.li``
-// const DividerLine = styled.div`
-//   background-color: ${props => props.theme.color.darkGray};
-//   width: 100%;
-//   height: 1px;
-//   margin: 2.8125rem 0rem 1.5rem;
-// `
+const DividerLine = styled.div`
+  background-color: ${props => props.theme.color.darkGray};
+  width: 100%;
+  height: 1px;
+  margin: 2.8125rem 0rem 1.5rem;
+`
 
 export default PlayerCredits
