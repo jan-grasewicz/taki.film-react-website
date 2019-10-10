5a9ffe724abb42f415a5cb0f4f8f179d1bac211e
jan-grasewicz <j.grasewicz@gmail.com>
2019-10-10 23:04:31 +0200

    LoseSidePadding refactored

--- src/elements/LoseSidePadding.js
+++ src/elements/LoseSidePadding.js
@@ -1,23 +1,18 @@
-import React from 'react'
 import styled from 'styled-components'
 
-const LoseSidePadding = ({ children, ...rest }) => (
-  <LosePadding {...rest}>{children}</LosePadding>
-)
-
-const LosePadding = styled.div`
+const LoseSidePadding = styled.div`
   position: relative;
 
-  width: calc(${props => props.theme.mainPadding.small} * 2 + 100%);
+  width: 100vw;
   left: -${props => props.theme.mainPadding.small};
 
   ${props => props.theme.media.tablet`
-      width: calc(${props => props.theme.mainPadding.medium} * 2 + 100%);
+      width: 100vw;
       left: -${props => props.theme.mainPadding.medium};  
     `}
 
   ${props => props.theme.media.desktop`
-      width: calc(${props => props.theme.mainPadding.large} * 2 + 100%);
+      width: 100vw;
       left: -${props => props.theme.mainPadding.large};  
     `}
 `
